25d8e432270c7d56952f8d71816bc024449c6d7f
Aditya Singh <adityasinghdragon6@gmail.com>
2026-04-15 23:18:34 +0530

    Delete exp9-main directory

--- exp9-main/backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectDB(mongoUri) {
-  if (!mongoUri) {
-    throw new Error("MONGO_URI is required.");
-  }
-  await mongoose.connect(mongoUri);
-}
--- exp9-main/backend/src/utils/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import { env } from "../config/env.js";
-
-export function signToken(userId) {
-  return jwt.sign({ id: userId }, env.jwtSecret, {
-    expiresIn: env.jwtExpiry
-  });
-}
